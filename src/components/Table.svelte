2ce9084ebccb6fc16d723b93b2f2f26ae334f714
Paul Brugger <paul@brugger.com>
2020-07-20 17:07:55 -0600

    pages and components

---
+++ src/components/Table.svelte
@@ -0,0 +1,1 @@
+<h1>the table</h1>
---
+++ src/components/TableContainer.svelte
@@ -0,0 +1,9 @@
+<script>
+  import TableFilter from "./TableFilter.svelte";
+  import Table from "./Table.svelte";
+</script>
+
+<h1>TableContainer</h1>
+
+<TableFilter />
+<Table />
