b2e2e7759db016f85ccafe47101a74d1beee7f66
Paul Brugger <paul@brugger.com>
2020-07-21 17:05:56 -0600

    5-about-page-titles

---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>About Covid 19 Tracker US</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>About</h1>
+    <p>
+      The data for this site came from
+      <a href="https://covidtracking.com">The Covid Tracking Project</a>
+      . You can learn more by visiting their site.
+    </p>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,16 @@
   import TableContainer from "../components/TableContainer.svelte";
 </script>
 
+<svelte:head>
+  <title>Covid 19 Tracker US</title>
+</svelte:head>
+
+<div class="section header">
+  <div class="container">
+    <h1>Covid 19 - US</h1>
+  </div>
+</div>
+
 <CovidStat />
 <CovidChart />
 <TableContainer />
